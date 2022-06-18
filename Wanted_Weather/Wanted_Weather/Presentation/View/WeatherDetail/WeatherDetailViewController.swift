39c633048b263b476917461f6f9cd197db66e494
chansenbbai <chanchu@hansung.ac.kr>
2022-06-19 03:39:07 +0900

    [#13] 데이터 파싱
    ### Description
    
    - 데이터 파싱

--- Wanted_Weather/Wanted_Weather/Presentation/View/WeatherDetail/WeatherDetailViewController.swift
+++ Wanted_Weather/Wanted_Weather/Presentation/View/WeatherDetail/WeatherDetailViewController.swift
@@ -8,15 +8,42 @@
 import UIKit
 
 final class WeatherDetailViewController: BaseViewController {
-
+    
     var viewModel = WeatherDetailViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        print(viewModel.weatherDetail.value)
-        
     }
     
-
+    override func bind() {
+        viewModel.weatherDetail.bind { value in
+            DispatchQueue.main.async {
+                value.forEach {
+                    $0.weather.forEach {
+                        print($0.icon)
+                        print($0.main)
+                        print($0.id)
+                        print($0.description)
+                    }
+                }
+                
+                value.forEach {
+                    print($0.main.feels_like)
+                    print($0.main.humidity)
+                    print($0.main.feels_like)
+                    print($0.main.pressure)
+                    print($0.main.temp)
+                    print($0.main.temp_max)
+                    print($0.main.temp_min)
+                }
+                
+                value.forEach {
+                    print($0.name)
+                    print($0.wind)
+                }
+            }
+        }
+    }
 }
+
+
